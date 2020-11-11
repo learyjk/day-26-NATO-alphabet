e5a93e6f0cb9b857ed37f9918a208f1d62de911c
keeganleary <leary.keegan@gmail.com>
2020-11-11 10:12:33 -0800

    Handle KeyError

--- main.py
+++ main.py
@@ -11,21 +11,26 @@ for (key, value) in student_dict.items():
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
     #Access index and row
     #Access row.student or row.score
     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 d = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-print([d[letter] for letter in word])
+
+
+def generate_phoenetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [d[letter] for letter in word]
+    except KeyError:
+        print("Please enter a valid word")
+        generate_phoenetic()
+    else:
+        print(output_list)
+
+
+generate_phoenetic()
